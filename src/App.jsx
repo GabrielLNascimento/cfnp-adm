4ba6c51fe7987dfb29b278041400c5eea969fae3
Gabriel De Liz Do Nascimento <gabriel.nascimento@renalvida.org.br>
2025-03-06 15:29:50 -0300

    import jwtdecoded

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes, Link, Navigate, useNavigate } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import './App.css';
 
 import UsuarioList from './components/UsuarioList';
